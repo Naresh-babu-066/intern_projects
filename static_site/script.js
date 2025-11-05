790bd8c049e66eafbf32ad7ad4931829b196f565
YELUSURI NARESH BABU <project1for2future3@gmail.com>
2025-11-05 10:24:42 +0530

    Add files via upload

---
+++ static_site/script.js
@@ -0,0 +1,9 @@
+// Toggle navigation menu for mobile
+function toggleMenu() {
+  document.querySelector('nav ul').classList.toggle('show');
+}
+
+// Basic interactivity
+function showAlert() {
+  alert('Welcome to My Website!');
+}

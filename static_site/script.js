bd2ef074ac90ccb69379d3baba6f4ee5c40037e8
YELUSURI NARESH BABU <project1for2future3@gmail.com>
2025-11-05 10:33:51 +0530

    Delete static_site directory

--- static_site/script.js
+++
@@ -1,9 +0,0 @@
-// Toggle navigation menu for mobile
-function toggleMenu() {
-  document.querySelector('nav ul').classList.toggle('show');
-}
-
-// Basic interactivity
-function showAlert() {
-  alert('Welcome to My Website!');
-}
